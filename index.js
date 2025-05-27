ed65118e533ae311ad42e6ef60c3dd2615dadecf
K M Marziul Karim <marziulrafi@gmail.com>
2025-05-27 23:43:33 +0600

    GET Jobs

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lpvyvs2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        // await client.connect();
+
+
+        const JobsCollection = client.db('CareerCode').collection('jobs')
+
+
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = JobsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Career Code')
+})
+
+app.listen(port, () => {
+    console.log(`Career Code server is running on port ${port}`)
+})
